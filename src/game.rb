5ee4959535c79b684f8c0aabcc9f339cc0a931cc
Victor David Santos <victordavidsantos@gmail.com>
2022-05-22 11:16:36 -0300

    Squares transition.

--- src/game.rb
+++ src/game.rb
@@ -4,6 +4,8 @@ require 'minigl'
 require_relative 'constants'
 require_relative 'screen'
 require_relative 'stats'
+require_relative 'menu'
+require_relative 'ui/transition'
 
 include MiniGL
 
@@ -31,7 +33,12 @@ class Game
     def start
       @font = TextHelper.new(Res.font(:font, 96), 0, @scale, @scale)
 
-      load_screen(1)
+      @controller = Menu.new
+    end
+    
+    def load_game
+      @controller.active = false
+      @transition = Transition.new(:squares, -> { load_screen(1) })
     end
 
     def load_screen(id, entrance_id = 0, transition = false)
@@ -39,39 +46,27 @@ class Game
       @screens[id] ||= screen
       if transition
         @controller.active = false
-        transition_screens(screen.reset(entrance_id))
+        @transition = Transition.new(:fade, lambda do
+          @controller = screen.reset(entrance_id)
+        end)
       else
         @controller = screen.reset(entrance_id)
-      end
-    end
-
-    def transition_screens(next_screen)
-      @transition_timer = 0
-      @on_transition_end = lambda do
-        @controller = next_screen
       end
     end
 
     def update
       G.window.close if KB.key_pressed?(Gosu::KB_ESCAPE)
 
-      if @transition_timer
-        @transition_timer += 1
-        if @transition_timer == 30
-          @on_transition_end.call
-        elsif @transition_timer == 60
-          @transition_timer = nil
-        end
+      if @transition
+        @transition.update
+        @transition = nil if @transition.dead?
       end
       @controller.update
     end
 
     def draw
       @controller.draw
-      return unless @transition_timer
-
-      alpha = ((@transition_timer >= 30 ? 60 - @transition_timer : @transition_timer).to_f / 30 * 255).round
-      Gosu.draw_rect(0, 0, @window_size.x, @window_size.y, alpha << 24, TRANSITION_Z_INDEX)
+      @transition&.draw
     end
   end
 end
---
+++ src/ui/transition.rb
@@ -0,0 +1,58 @@
+require_relative '../constants'
+
+class Transition
+  def initialize(type, on_halfway = nil, on_end = nil)
+    @type = type
+    @on_halfway = on_halfway
+    @on_end = on_end
+    @timer = 0
+    @half_duration, @full_duration =
+      case @type
+      when :squares then [60, 120]
+      else               [30, 60]
+      end
+    
+    if @type == :squares
+      @square_size = 80 * Game.scale
+      @cols = (Game.window_size.x.to_f / @square_size).ceil
+      @rows = (Game.window_size.y.to_f / @square_size).ceil
+      @slope = (@half_duration + @cols + @rows).to_f / @half_duration
+    end
+  end
+  
+  def dead?
+    @timer >= @full_duration
+  end
+  
+  def update
+    @timer += 1
+    if @timer == @half_duration
+      @on_halfway&.call
+    elsif @timer == @full_duration
+      @on_end&.call
+    end
+  end
+  
+  def draw
+    case @type
+    when :fade
+      alpha = ((@timer >= 30 ? 60 - @timer : @timer).to_f / 30 * 255).round
+      Gosu.draw_rect(0, 0, Game.window_size.x, Game.window_size.y, alpha << 24, TRANSITION_Z_INDEX)
+    when :squares
+      (0...@cols).each do |i|
+        (0...@rows).each do |j|
+          size =
+            if @timer >= @half_duration
+              [[-@slope * (@timer - @half_duration) + @half_duration + i + j, 0].max / @half_duration, 1].min * @square_size
+            else
+              [[@slope * @timer - i - j, 0].max / @half_duration, 1].min * @square_size
+            end
+          offset = (@square_size - size) / 2
+          Gosu.draw_rect(i * @square_size + offset,
+                         j * @square_size + offset,
+                         size, size, 0xff000000, TRANSITION_Z_INDEX)
+        end
+      end
+    end
+  end
+end
